074005faa3731ce20689a7ec5183084d91dede75
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-01-22 17:57:03 +0530

    added login and checker in user collection based on jwt token having epiration of 5 minutes

--- src/main/java/com/adityavikas/codeverse/controllers/PublicController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/PublicController.java
@@ -1,10 +1,16 @@
 package com.adityavikas.codeverse.controllers;
 
+import com.adityavikas.codeverse.config.JwtConfig;
 import com.adityavikas.codeverse.entity.User;
+import com.adityavikas.codeverse.services.UserDetailsServiceImpl;
 import com.adityavikas.codeverse.services.UserService;
+import com.adityavikas.codeverse.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +19,15 @@ public class PublicController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/health-check")
     public String checkHealh(){
@@ -32,6 +47,21 @@ public class PublicController {
         }
         catch(Exception e){
             return new ResponseEntity<>("Error : User not Registered", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword())
+            );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+            String jwt = jwtUtils.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>("Username or password not correct",HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/adityavikas/codeverse/controllers/UserController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Component
 @RestController
@@ -19,9 +16,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(){
-        return null;
+    @GetMapping("/isWorking")
+    public String isWorking(){
+        return "Yes It is working";
     }
 
 }
