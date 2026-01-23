6e6f5414a39f4710f06398071dd69acf7ee68978
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-01-23 23:38:31 +0530

    added admin controller

--- src/main/java/com/adityavikas/codeverse/controllers/AdminController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/AdminController.java
@@ -2,6 +2,8 @@ package com.adityavikas.codeverse.controllers;
 
 import com.adityavikas.codeverse.entity.User;
 import com.adityavikas.codeverse.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@Tag(name="All Admin API's",description = "this is the admin controller where only admin can do the neccessary change to our codeverse system application not used is allowed.")
 public class AdminController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "this is used to create admin (Note: only a admin can create other admin)")
     // only one admin can create other admin not user is permitted to call this you can try it by yourself
     @PostMapping("/create-admin")
     public ResponseEntity<?> createAdmin(@RequestBody User admin){
