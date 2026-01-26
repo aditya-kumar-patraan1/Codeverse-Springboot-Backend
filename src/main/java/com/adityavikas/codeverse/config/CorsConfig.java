f861c9a03668c7c2ae8a37a0a20cb2bbf82b04be
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-01-26 20:26:10 +0530

    added https://code-arena-seven-ebon.vercel.app/ in cors config origin

--- src/main/java/com/adityavikas/codeverse/config/CorsConfig.java
+++ src/main/java/com/adityavikas/codeverse/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://code-arena-seven-ebon.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowCredentials(true);
