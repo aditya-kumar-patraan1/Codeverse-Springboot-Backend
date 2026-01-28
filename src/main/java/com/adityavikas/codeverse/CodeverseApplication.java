efb8cd917abb4b0fba8e84234ac39459ee3eaffb
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-01-28 21:21:08 +0530

    corrected MONGO_URI to MONGODB_URI

--- src/main/java/com/adityavikas/codeverse/CodeverseApplication.java
+++ src/main/java/com/adityavikas/codeverse/CodeverseApplication.java
@@ -2,6 +2,7 @@ package com.adityavikas.codeverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
@@ -12,8 +13,9 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class CodeverseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeverseApplication.class, args);
-	}
+        ConfigurableApplicationContext context = SpringApplication.run(CodeverseApplication.class,args);
+        System.out.println(context.getEnvironment());
+  	}
 
     @Bean
     public PlatformTransactionManager falana(MongoDatabaseFactory factory){
