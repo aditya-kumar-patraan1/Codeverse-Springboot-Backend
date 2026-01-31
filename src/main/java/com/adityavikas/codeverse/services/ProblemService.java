b1789b998a1513ec93bc89bcf1e17362de32a697
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-01-31 23:47:36 +0530

    added fetch all problem service using problemRepositroy

--- src/main/java/com/adityavikas/codeverse/services/ProblemService.java
+++ src/main/java/com/adityavikas/codeverse/services/ProblemService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ProblemService {
@@ -30,7 +31,7 @@ public class ProblemService {
     }
 
     public fetchAllProblems(){
-
+        return problemRepository.findAll();
     }
 
 }
