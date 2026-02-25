4252a89ed063651d5eecb1cd089d4adbd2069526
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-02-25 21:47:22 +0530

    ProblemDetail entity added

---
+++ src/main/java/com/adityavikas/codeverse/entity/ProblemDetails.java
@@ -0,0 +1,25 @@
+package com.adityavikas.codeverse.entity;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Document(collection = "ProblemDetails")
+public class ProblemDetails {
+
+    @Id
+    private ObjectId id;
+    private ObjectId problemId;
+    private String description;
+    private String editorial;
+    private Map<String,String> templates = new HashMap<>();
+    private Map<String,String> solutions = new HashMap<>();
+
+
+
+}
