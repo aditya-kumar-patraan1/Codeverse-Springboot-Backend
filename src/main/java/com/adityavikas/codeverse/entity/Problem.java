492488257eaad0dda7a3b021df09190c42164afd
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-03-06 18:58:15 +0530

    added @JsonSerialize in Problem entity

--- src/main/java/com/adityavikas/codeverse/entity/Problem.java
+++ src/main/java/com/adityavikas/codeverse/entity/Problem.java
@@ -1,8 +1,12 @@
 package com.adityavikas.codeverse.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -17,6 +21,7 @@ import java.util.List;
 public class Problem {
 
     @Id
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;   //mongo unique id assigned auto by atlas
 
     @Indexed(unique = true)
