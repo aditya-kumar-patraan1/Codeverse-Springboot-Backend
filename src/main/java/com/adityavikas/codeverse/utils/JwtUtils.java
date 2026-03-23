2076bc8cc3489bb4d47d60c65faf309c94420b03
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-03-23 18:55:48 +0530

    jwt expiration time extends to 2 days

--- src/main/java/com/adityavikas/codeverse/utils/JwtUtils.java
+++ src/main/java/com/adityavikas/codeverse/utils/JwtUtils.java
@@ -55,7 +55,7 @@ public class JwtUtils {
                 .header().empty().add("typ","JWT")
                 .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour expiration time
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12 * 2)) // 2 days expiration time
                 .signWith(getSigningKey())
                 .compact();
     }
