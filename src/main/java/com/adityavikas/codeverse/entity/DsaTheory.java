c7f130904f92b5edfb19e2abc712c39d5949fbe1
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-04-27 22:28:49 +0530

    added DsaTitle & DsaTheory entity

---
+++ src/main/java/com/adityavikas/codeverse/entity/DsaTheory.java
@@ -0,0 +1,14 @@
+package com.adityavikas.codeverse.entity;
+
+import lombok.Data;
+
+@Data
+public class DsaTheory {
+
+    private String parentTitleId;
+    private String title;
+    private String level;
+    private String icon;
+
+
+}
--- src/main/java/com/adityavikas/codeverse/entity/DsaTitle.java
+++ src/main/java/com/adityavikas/codeverse/entity/DsaTitle.java
@@ -1,13 +1,17 @@
 package com.adityavikas.codeverse.entity;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Document("DsaTitle")
 public class DsaTitle {
 
     private String title;
-    private String listOfIds;
+    private List<ObjectId> listOfIds = new ArrayList<>();
 
 }
