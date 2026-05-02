30c26ff43925c1f1ed9c9be7a6b53536e965fd5a
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-05-02 23:36:36 +0530

    added DSATemplate controller Signature API endpoint

--- src/main/java/com/adityavikas/codeverse/controllers/DsaTemplateController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/DsaTemplateController.java
@@ -1,4 +1,11 @@
 package com.adityavikas.codeverse.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DsaTemplateController")
+@Tag(name="Dsa Template API Controller",description = "This is the controller added for handling the operation related to DsaTemplate")
 public class DsaTemplateController {
 }
