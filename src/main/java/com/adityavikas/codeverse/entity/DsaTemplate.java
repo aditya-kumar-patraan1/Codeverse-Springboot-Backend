6b60839e909065863db86c1cf1fd9e9494f28834
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-05-15 01:19:28 +0530

    changes some setting in DsaTemplate Entity

--- src/main/java/com/adityavikas/codeverse/entity/DsaTemplate.java
+++ src/main/java/com/adityavikas/codeverse/entity/DsaTemplate.java
@@ -11,16 +11,15 @@ import java.util.List;
 @Document(collection = "DsaTemplate")
 public class DsaTemplate {
 
-
-    private String templateName;
-    private ObjectId titleId;
+    private String parentTitle;
+    private ObjectId title;
     private String templateId;
-    private List<String> practiceProblemLinks = new ArrayList<>();
-    private List<String> youtubeLinks = new ArrayList<>();
+    private List<String> problemLinks = new ArrayList<>();
+    private List<String> videoLinks = new ArrayList<>();
     private boolean status;
     private String cpp;
     private String java;
-    private String js;
+    private String javascript;
     private String python;
 
 }
