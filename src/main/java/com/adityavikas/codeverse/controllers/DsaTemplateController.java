c1d7146f3f2ab87a7bea339a4c05029934acec79
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-05-14 23:40:45 +0530

    added dsa template adding controller endpoint

--- src/main/java/com/adityavikas/codeverse/controllers/DsaTemplateController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/DsaTemplateController.java
@@ -1,6 +1,10 @@
 package com.adityavikas.codeverse.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/DsaTemplateController")
 @Tag(name="Dsa Template API Controller",description = "This is the controller added for handling the operation related to DsaTemplate")
 public class DsaTemplateController {
+
+    @PostMapping("/addTemplate/{title}")
+    public ResponseEntity<?> addDsaTemplate(@PathVariable String title){
+        return new ResponseEntity<>("title is : "+title,HttpStatus.OK);
+    }
+
 }
