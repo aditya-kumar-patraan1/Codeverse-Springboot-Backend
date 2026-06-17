54c3a689a7825519f088cb91c361d07acefb89b1
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-17 23:46:12 +0530

    added getJdoodleConfig

--- src/main/java/com/adityavikas/codeverse/utils/CodeEexecutionUtils.java
+++ src/main/java/com/adityavikas/codeverse/utils/CodeEexecutionUtils.java
@@ -1,4 +1,39 @@
 package com.adityavikas.codeverse.utils;
 
+import com.adityavikas.codeverse.dto.LanguageFormatDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeEexecutionUtils {
+
+    public LanguageFormatDTO getJdoodleConfig(String language){
+
+        List<List<String>> listofLanguages = new ArrayList<>();
+        listofLanguages.add(new ArrayList<>(List.of("python","python3")));
+        listofLanguages.add(new ArrayList<>(List.of("cpp","c++")));
+        listofLanguages.add(new ArrayList<>(List.of("js","javascript")));
+        listofLanguages.add(new ArrayList<>(List.of("java")));
+
+        int idx=0;
+
+        for(var currList : listofLanguages){
+            if(currList.contains(language.toLowerCase())){
+                break;
+            }
+            idx++;
+        }
+
+        List<List<String>> versionData = new ArrayList<>();
+        versionData.add(List.of("python3","3"));
+        versionData.add(List.of("cpp","5"));
+        versionData.add(List.of("nodejs","4"));
+        versionData.add(List.of("java","4"));
+
+        List<String> finalData = versionData.get(idx);
+
+        return new LanguageFormatDTO(finalData.get(0), finalData.get(1));
+
+    }
+
 }
