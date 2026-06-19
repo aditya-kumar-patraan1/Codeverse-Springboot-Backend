a4df403afb910807b6ac3ba3a5bfbd84fc6f66e1
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-19 18:14:30 +0530

    added function namefindByContestId in ProblemRepository

--- src/main/java/com/adityavikas/codeverse/repository/ProblemRepository.java
+++ src/main/java/com/adityavikas/codeverse/repository/ProblemRepository.java
@@ -4,8 +4,12 @@ import com.adityavikas.codeverse.entity.Problem;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProblemRepository extends MongoRepository<Problem, ObjectId> {
 
     Problem findBySlug(String slug);
+
+    List<Problem> findByContestId(ObjectId contestId);
 
 }
