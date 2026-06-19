82c3460334ca67e3d9bb21973b048648d550c4b5
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-19 18:39:17 +0530

    ContestProblemDTO added

--- src/main/java/com/adityavikas/codeverse/dto/ContestProblemDTO.java
+++ src/main/java/com/adityavikas/codeverse/dto/ContestProblemDTO.java
@@ -1,4 +1,34 @@
 package com.adityavikas.codeverse.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class ContestProblemDTO {
+
+    private int sno;
+
+    @NotNull
+    private String title;
+    private String slug;
+    String topicTags;
+    private String difficulty;
+    private double acceptanceRate = 0;
+    private String inputType;
+    private String returnType;
+    private String functionName;
+    private int problemOrder;
+    private String description = "";
+    private String editorial = "";
+    private Map<String, String> templates = new HashMap<>();
+    private Map<String, String> solutions = new HashMap<>();
+    private Map<String, String> timeComplexity = new HashMap<>();
+    private Map<String, String> spaceComplexity = new HashMap<>();
+    private List<String> algorithmSteps = new ArrayList<>();
+    private List<TestcaseDTO> testCases = new ArrayList<>();
 }
