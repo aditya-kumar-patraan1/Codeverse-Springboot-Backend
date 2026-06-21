10680292d251a25802d28ee622346000d219f861
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-21 22:12:49 +0530

    added parameters to addContestProblem()

---
+++ src/main/java/com/adityavikas/codeverse/api/response/JdoodleResponse.java
@@ -0,0 +1,4 @@
+package com.adityavikas.codeverse.api.response;
+
+public class JdoodleResponse {
+}
--- src/main/java/com/adityavikas/codeverse/services/ContestProblemService.java
+++ src/main/java/com/adityavikas/codeverse/services/ContestProblemService.java
@@ -1,14 +1,17 @@
 package com.adityavikas.codeverse.services;
 
+import com.adityavikas.codeverse.dto.ContestProblemDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ContestProblemService {
 
-    public boolean addContestProblem(){
+    public boolean addContestProblem(ObjectId contestId, ContestProblemDTO contestProblemDTO){
         try{
+
             return true;
         } catch (Exception e) {
             log.error("contest problem not added & rollback...");
