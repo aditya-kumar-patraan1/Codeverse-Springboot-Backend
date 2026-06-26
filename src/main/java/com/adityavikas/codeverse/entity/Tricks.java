01c6fdee2bc3a9fb2e501c39e3bfb435af770609
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-26 22:02:52 +0530

    trick entity added

---
+++ src/main/java/com/adityavikas/codeverse/entity/Tricks.java
@@ -0,0 +1,16 @@
+package com.adityavikas.codeverse.entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "Tricks")
+@Data
+public class Tricks {
+
+    private String notes;
+    private LocalDateTime updated_at;
+    private LocalDateTime created_at;
+
+}
