a285e404c53b55eb0f0f3b8dbeaa9060b43bef62
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2026-06-26 23:26:39 +0530

    added update notes

--- src/main/java/com/adityavikas/codeverse/controllers/TrickController.java
+++ src/main/java/com/adityavikas/codeverse/controllers/TrickController.java
@@ -2,6 +2,8 @@ package com.adityavikas.codeverse.controllers;
 
 import com.adityavikas.codeverse.entity.Trick;
 import com.adityavikas.codeverse.services.TrickService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/trick")
+@Tag(name="Trick Controller",description = "This Controller is used to add/update or clear the trick that are repeatedly used in DSA/CP.")
 public class TrickController {
 
     @Autowired
@@ -19,6 +22,7 @@ public class TrickController {
 
 
     @PutMapping("/update")
+    @Operation(summary = "This API Ednpoint is used to add/update/clear the trick note")
     public ResponseEntity<?> updateNote(@RequestBody Trick updatedNote){
         Map<String,Integer> returnResponse = new HashMap<>();
         boolean isNotesUpdated = trickService.updateTrick(updatedNote.getNote());
